8f24c0b2e8cdcacca3d881807142d9755e48394a
Hafiz Bilal <hafiz.bilal@brainplow.com>
2023-06-14 15:15:45 +0500

    first

--- AppCenterProject/AppDelegate.swift
+++ AppCenterProject/AppDelegate.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +17,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        AppCenter.start(withAppSecret: "28ec3c0b-213d-4cd0-9ea5-10b819d95642", services:[
+          Analytics.self,
+          Crashes.self
+        ])
+        
+        
+//        AppCenter.start(withAppSecret: "28ec3c0b-213d-4cd0-9ea5-10b819d95642", services:[
+//          Analytics.self,
+//          Crashes.self
+//        ])
+
+        
         return true
     }
 
---
+++ Pods/Target Support Files/Pods-AppCenterProject/Pods-AppCenterProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppCenterProject : NSObject
+@end
+@implementation PodsDummy_Pods_AppCenterProject
+@end
